b0fda60353ab1dc444425b242f6d5a354cd74c33
Daniel Fimaini <daniel.fimiani@mercadolibre.com>
2024-05-17 22:09:37 -0300

    add concillied data for serach

--- src/api/core/contracts/search/response.go
+++ src/api/core/contracts/search/response.go
@@ -17,6 +17,9 @@ type SearchResponse struct {
 	IdentificationNumber string                       `json:"payer_identification_number"`
 	CurrencyID           constants.Currency           `json:"currency_id"`
 	TransactionAmount    decimal.Decimal              `json:"transaction_amount"`
+	ConcilliedStatus     *constants.ConcilliedStatus  `json:"concillied_status"`
+	ConcilliedDate       *time.Time                   `json:"concillied_date"`
+	ConcilliedUser       *string                      `json:"concillied_user"`
 }
 
 func NewSearchResponse(payments []entities.Payment) []SearchResponse {
@@ -32,6 +35,9 @@ func NewSearchResponse(payments []entities.Payment) []SearchResponse {
 			IdentificationNumber: payment.Payer.Number,
 			CurrencyID:           payment.CurrencyID,
 			TransactionAmount:    payment.TransactionAmount,
+			ConcilliedStatus:     &payment.ConcilliedStatus,
+			ConcilliedDate:       payment.ConcilliedDate,
+			ConcilliedUser:       payment.ConcilliedUser,
 		}
 
 		response[i] = res
